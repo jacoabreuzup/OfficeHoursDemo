c6ed02246cdcbbf846205c35a5acadad721f527e
jacoabreuzup <joao.abreu@zup.com.br>
2021-05-04 15:07:24 -0300

    starter point for office hours

--- bff/src/main/kotlin/com/example/bff/service/DetailsZipCodeService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bff.service
-
-import com.example.bff.builder.DetailsZipCodeScreenBuilder
-import org.springframework.stereotype.Service
-
-@Service
-class DetailsZipCodeService {
-    fun createDetailsZipCodeScreen() = DetailsZipCodeScreenBuilder
-}
--- bff/src/main/kotlin/com/example/bff/service/ZipCodeService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bff.service
-
-import com.example.bff.builder.ZipCodeScreenBuilder
-import org.springframework.stereotype.Service
-
-@Service
-class ZipCodeService {
-    fun createZipCodeScreen() = ZipCodeScreenBuilder
-}
